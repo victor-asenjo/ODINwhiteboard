60f3eb06cc0ac6a31237992d0039405967c0d655
Víctor Asenjo <victorasenj@gmail.com>
2023-04-19 14:23:53 +0200

    Initial commit

---
+++ odin/src/main/java/edu/upc/essi/dtim/odin/OdinApplication.java
@@ -0,0 +1,14 @@
+package edu.upc.essi.dtim.odin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdinApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(OdinApplication.class, args);
+    }
+
+}
