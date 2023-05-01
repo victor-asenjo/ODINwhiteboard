8ae69d6d3170881949c83b5614789c3d518d5980
Víctor Asenjo <victorasenj@gmail.com>
2023-05-01 17:51:01 +0200

    credentials protected

--- ODINwhiteboard/odin/src/main/java/edu/upc/essi/dtim/odin/config/JpaConfiguration.java
+++ ODINwhiteboard/odin/src/main/java/edu/upc/essi/dtim/odin/config/JpaConfiguration.java
@@ -9,20 +9,43 @@ import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 @Configuration
 @EnableJpaRepositories("edu.upc.essi.dtim.odin.NextiaStore.ORMStore")
+@EnableTransactionManagement
 public class JpaConfiguration {
+    private String username;
+    private String password;
+    private String url;
+
+    void getCredentials(){
+        Properties properties = new Properties();
+        try {
+            FileInputStream fis = new FileInputStream("..\\ODINwhiteboard\\odin\\src\\main\\resources\\credentials.properties");
+            properties.load(fis);
+            username = properties.getProperty("db.username");
+            password = properties.getProperty("db.password");
+            url = properties.getProperty("db.url");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Bean
     public DataSource dataSource() {
+        getCredentials();
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/ORMDB");
-        dataSource.setUsername( "username" );
-        dataSource.setPassword( "password" );
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
         return dataSource;
     }
 
