3df847d574ec443cdf3ec747fbecfe76ab937ad9
Víctor Asenjo <victorasenj@gmail.com>
2023-05-01 19:11:36 +0200

    hiding credentials working

--- ODINwhiteboard/odin/src/main/java/edu/upc/essi/dtim/odin/OdinApplication.java
+++ ODINwhiteboard/odin/src/main/java/edu/upc/essi/dtim/odin/OdinApplication.java
@@ -2,8 +2,10 @@ package edu.upc.essi.dtim.odin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:credentials.properties")
 public class OdinApplication {
 
 	public static void main(String[] args) {
