96a60d96d967612692d6cf57d5782327f953f184
Víctor Asenjo <victorasenj@gmail.com>
2023-05-15 17:16:02 +0200

    Update projects.store.js DELETE A PROJECT DONE AND WORKING

--- frontend/src/stores/projects.store.js
+++ frontend/src/stores/projects.store.js
@@ -11,10 +11,6 @@ export const useProjectsStore = defineStore('projects',{
 
     getters : {},
     actions: {
-      // initStores(){
-      //  authStore = useAuthStore();
-      //
-      // },
       init() {
         console.log("projects store init")
         // this.initStores();
@@ -50,7 +46,7 @@ export const useProjectsStore = defineStore('projects',{
         project.createdBy = "Julio Berne"//authStore.user.username
         console.log("send project: ", project)
         projectAPI.createProject(project, authStore.user.accessToken).then((response) => {
-          if (response.status == 201) {
+          if (response.status === 201) {
             console.log(response)
             notify.positive(`Project ${project.name} successfully created`)
             this.projects.push(response.data)
@@ -72,10 +68,15 @@ export const useProjectsStore = defineStore('projects',{
         projectAPI.deleteProjectByID(id, authStore.user.accessToken)
           .then((response) => {
             if (response.status === 200) {
-              window.location.reload(); // Reload the page to reflect the changes
-              notify.positive(`Project ${id} successfully deleted`);
-
-              successCallback();
+              const index = this.projects.findIndex(project => project.projectId === id);
+              if (index !== -1) {
+                // Remove the project from the projects array using splice
+                this.projects.splice(index, 1);
+                notify.positive(`Project ${id} successfully deleted`);
+                successCallback();
+              } else {
+                notify.negative("Project not found in the list.");
+              }
             } else {
               notify.negative("Cannot delete project. Something went wrong on the server.");
             }
@@ -91,96 +92,3 @@ export const useProjectsStore = defineStore('projects',{
 
 
     })
-
-
-
-// import { defineStore } from 'pinia'
-// import {useNotify} from 'src/use/useNotify.js'
-// import projectAPI from "src/api/projectAPI.js";
-// import { useAuthStore } from 'stores/auth.store.js'
-
-// // let notify;
-// // let authStore;
-
-// export const useProjectsStore = defineStore('projects',{
-
-//     state: () => ({
-//         projects : [],
-//     }),
-
-//     getters : {},
-//     actions: {
-//         // initStores(){
-//             //  authStore = useAuthStore();
-//             //  notify = useNotify();
-//         // },
-//         init(){
-//             console.log("projects store init")
-//             // this.initStores();
-//             const authStore = useAuthStore();
-//             if(authStore.user.accessToken && this.projects.length === 0) {
-//                 this.getProjects()
-//             }
-
-//         },
-//         getProjects(){
-//             const authStore = useAuthStore();
-//             projectAPI.getAllProjects(authStore.user.accessToken)
-//                             .then(response => {
-
-//                                 console.log("projects received")
-//                                 console.log(response.data)
-
-//                                 if(response.data === "") { // when no datasources, api answer ""
-//                                     this.projects = []
-//                                 } else {
-//                                     this.projects = response.data
-//                                 }
-
-//                             }).catch(err => {
-//                             console.log("error retrieving data sources")
-//                             console.log(err)
-//                             })
-//         },
-//         createProject(project, successCallback){
-//             console.log("create project store...")
-
-//             // console.log(authStore.user)
-//             project.createdBy = authStore.user.username
-//             console.log("send project: ",project)
-//             projectAPI.createProject(project, authStore.user.accessToken ).then((response) => {
-//                 if (response.status == 201) {
-
-//                     console.log(response)
-//                   notify.positive(`Project ${project.name} successfully created`)
-//                   // onReset()
-//                 //
-//                     this.projects.push(response.data)
-//                   successCallback()
-
-//                 } else {
-//                   // console.log("error")
-//                   notify.negative("Cannot create project. Something went wrong in the server.")
-//                 }
-//               }).catch( (error) => {
-
-//                 console.log("error is: "+error)
-//                 if(error.response){
-//                     notify.negative("Something went wrong in the server for creating a project.")
-//                 }
-
-
-//             });
-
-
-
-
-
-//         }
-
-
-//     }
-
-
-
-// })
